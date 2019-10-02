9b029428e1df4dfc571fd05662a3921de2ac477e
RenatoViana <renato_viana@icloud.com>
2019-10-02 19:00:39 +0100

    Created prompt activity
    Launches promp activity when no file permission is given. The prompt activity directs the user to the settings menu and exits the app.

---
+++ app/src/main/java/theredspy15/ltecleanerfoss/PromptActivity.java
@@ -0,0 +1,32 @@
+package theredspy15.ltecleanerfoss;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.Button;
+
+public class PromptActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prompt);
+
+        Button button = findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivityForResult(new Intent(android.provider.Settings.ACTION_SETTINGS), 0);
+                System.exit(0);
+            }
+        });
+    }
+
+}
